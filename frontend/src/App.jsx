bb2b5c234100910440e34aa61c13b82ba9914557
Keengsleyudeh <keengsleyudeh@gmail.com>
2025-05-05 22:04:02 +0100

    refactor: update frontend dependencies and improve build process
    - Updated React and React DOM to version 19.1.0 in package.json.
    - Simplified build script in root package.json for better readability.
    - Removed Appv2.jsx and integrated its functionality into App.jsx.
    - Enhanced ESLint configuration for better code quality checks.
    - Added .gitignore file to exclude unnecessary files from version control.
    - Minor adjustments to component exports for consistency.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,11 +19,7 @@ function App() {
 		queryKey: ["authUser"],
 		queryFn: async () => {
 			try {
-				const res = await fetch("http://localhost:5000/api/auth/me",
-                    {
-                        credentials: "include",
-                    }
-                );
+				const res = await fetch("/api/auth/me");
 				const data = await res.json();
 				if (data.error) return null;
 				if (!res.ok) {
